452de7f5280fef245172613461ee271c6ffcaefa
ACRL <angryaccelerated@qq.com>
2021-09-22 22:37:08 +0800

    renderer header/source separation

--- include/render/renderer.h
+++ include/render/renderer.h
@@ -9,18 +9,5 @@
 
 namespace Physics2D
 {
-	//class Renderer
-	//{
-	//public:
-	//	virtual void renderPolygon(const ShapePrimitive& shape) = 0;
-	//	virtual void renderRectangle(const ShapePrimitive& shape) = 0;
-	//	virtual void renderEllipse(const ShapePrimitive& shape) = 0;
-	//	virtual void renderEdge(const ShapePrimitive& shape) = 0;
-	//	virtual void renderCenterPoint(const ShapePrimitive& shape) = 0;
-	//	virtual void renderCircle(const ShapePrimitive& shape) = 0;
-	//	virtual void renderPoint(const Vector2& pos) = 0;
-	//	virtual void renderLine(const Vector2& p1, const Vector2& p2) = 0;
-	//};
-
 }
 #endif
