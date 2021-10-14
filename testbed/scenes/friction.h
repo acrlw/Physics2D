0e432d8ef0ce9474180672d7bcc3b26f609a2853
ACRL <2359769683@qq.com>
2021-10-14 11:39:52 +0800

    friction demo

--- testbed/scenes/friction.h
+++ testbed/scenes/friction.h
@@ -13,6 +13,52 @@ namespace Physics2D
 		}
 		void load() override
 		{
+			Edge edge;
+			edge.set({ 0, 0 }, { 10, 0 });
+			edge_ptr = std::make_unique<Edge>(edge);
+
+			Edge ramp;
+			ramp.set({ -10, 4 }, { 0, 0 });
+			ramp_ptr = std::make_unique<Edge>(ramp);
+
+
+			Rectangle rect(0.5f, 0.5f);
+			rectangle_ptr = std::make_unique<Rectangle>(rect);
+
+			for(real i = 0;i < 3.0f;i += 1.0f)
+			{
+				Body* ground = m_world->createBody();
+				ground->setShape(edge_ptr.get());
+				ground->setFriction(0.1f);
+				ground->setMass(Constant::Max);
+				ground->position().set(0, i * 3.0f);
+				ground->setRestitution(0);
+				ground->setType(Body::BodyType::Static);
+
+				Body* ramp = m_world->createBody();
+				ramp->setShape(ramp_ptr.get());
+				ramp->setFriction(0.1f);
+				ramp->setMass(Constant::Max);
+				ramp->position().set(0, i * 3.0f);
+				ramp->setRestitution(0);
+				ramp->setType(Body::BodyType::Static);
+
+				m_tree->insert(ground);
+				m_tree->insert(ramp);
+			}
+
+			for (real i = 1; i < 4.0f; i += 1.0f)
+			{
+				Body* cube = m_world->createBody();
+				cube->setShape(rectangle_ptr.get());
+				cube->setFriction(i * 0.3f);
+				cube->setMass(1);
+				cube->position().set(-5.0f, i * 3.5f);
+				cube->setRestitution(0);
+				cube->setType(Body::BodyType::Dynamic);
+				m_tree->insert(cube);
+			}
+
 
 		}
 		void render(QPainter* painter) override
